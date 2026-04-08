8d1e321ce7ba815c08989f4585e9f7c6dcc9a80c
Gab <gabriel.jamet@edu.ece.fr>
2026-04-08 14:27:25 +0200

    caca

--- src/vue/Fenetre.java
+++ src/vue/Fenetre.java
@@ -1,6 +1,8 @@
 package vue;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Fenetre {
     public static void main(String[] args) {
@@ -51,29 +53,72 @@ public class Fenetre {
         backgroundLabel.add(btnAdminLabel);
         backgroundLabel.add(btnQuitterLabel);
 
-        //Evenement pour que quanson clique sur le bouton quitter ca quitte
-        btnQuitterLabel.addMouseListener(new java.awt.event.MouseAdapter() {
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent e) {
-                frame.dispose();
-            }
 
+        // EVENEMENTS
+
+        //Evenement pour que quand on clique sur le bouton quitter ca quitte
+        MouseAdapter listenerQuitter = new MouseAdapter() {
             @Override
-            public void mouseEntered(java.awt.event.MouseEvent e) {
-                btnQuitterLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            public void mouseClicked(MouseEvent e) { frame.dispose(); } // si cliquer sur btn fermer la fenêtre
+            @Override
+            public void mouseEntered(MouseEvent e) { btnQuitterLabel.setCursor(new Cursor(Cursor.HAND_CURSOR)); }// le curseur se tranfome en main au dessus du bouton
+        };
+
+        //Evenement pour le bouton admin
+        MouseAdapter listenerAdmin = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {  } // si cliquer sur btn fermer la fenêtre
+            @Override
+            public void mouseEntered(MouseEvent e) { btnAdminLabel.setCursor(new Cursor(Cursor.HAND_CURSOR)); }// le curseur se tranfome en main au dessus du bouton
+        };
+
+        //Evenement pour le bouton Creer un compte/Connexion
+        MouseAdapter listenerConnexion = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {  } // si cliquer sur btn fermer la fenêtre
+            @Override
+            public void mouseEntered(MouseEvent e) { btnConnexionLabel.setCursor(new Cursor(Cursor.HAND_CURSOR)); }// le curseur se tranfome en main au dessus du bouton
+        };
+
+        //Evenement pour le bouton LOGO
+        MouseAdapter listenerLogo = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {  } // si cliquer sur btn fermer la fenêtre
+            @Override
+            public void mouseEntered(MouseEvent e) { logoLabel.setCursor(new Cursor(Cursor.HAND_CURSOR)); }// le curseur se tranfome en main au dessus du bouton
+        };
+
+        // On ajoute le listener au bouton
+        btnQuitterLabel.addMouseListener(listenerQuitter);
+        btnAdminLabel.addMouseListener(listenerAdmin);
+        btnConnexionLabel.addMouseListener(listenerConnexion);
+        logoLabel.addMouseListener(listenerLogo);
+
+
+        //Evenement pour deplacer le bandeau comme une vrai fenetre
+
+        final Point[] position_fenetre = {null}; // Variables pour stocker la position initiale
+
+        MouseAdapter listenerBandeau = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                position_fenetre[0] = e.getPoint(); // position ou j'ai cliqué dans le bandeau avec la souris
             }
-        });
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Point coordonees_actuels = e.getLocationOnScreen(); // position sur l'écran
+                frame.setLocation(coordonees_actuels.x - position_fenetre[0].x,coordonees_actuels.y - position_fenetre[0].y);
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) { bandeauLabel.setCursor(new Cursor(Cursor.HAND_CURSOR)); }// le curseur se tranfome en main au dessus du bouton
+        };
+
+        bandeauLabel.addMouseListener(listenerBandeau);
+        bandeauLabel.addMouseMotionListener(listenerBandeau);
+
+
+
 
         frame.setVisible(true);
-
-
-        //JLabel infoLabel = new JLabel("Logo Width: " + bandeau.getIconWidth() + " Height: " + bandeau.getIconHeight());
-        //infoLabel.setBounds(20, 20, 300, 30); // Position en haut à gauche
-        //infoLabel.setForeground(java.awt.Color.RED); // Visible sur fond
-        //backgroundLabel.add(infoLabel);
-
-
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
 }
