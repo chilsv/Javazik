b3cc9e107d4176e00ee3842ca72eeec1d8656293
Gab <gabriel.jamet@edu.ece.fr>
2026-04-09 14:43:51 +0200

    Finalisation des modifs locales

--- src/controleur/Main.java
+++ src/controleur/Main.java
@@ -5,14 +5,9 @@ import vue.FenetreMenu;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
         FenetreMenu fenetre = new FenetreMenu();
         Evenements.ajouterEvenements(fenetre);
         fenetre.afficher();
-
-
     }
 
 
